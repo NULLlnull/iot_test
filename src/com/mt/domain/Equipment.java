e9b069b7fff5b2f21ef445d9996805cb5f4952f0
PC <954356394@qq.com>
2019-03-04 20:23:40 +0800

    Initial commit

---
+++ src/com/mt/domain/Equipment.java
@@ -0,0 +1,73 @@
+package com.mt.domain;
+
+public class Equipment {
+    private Integer id;
+
+    private String mac;
+
+    private String name;
+
+    private String classs;
+
+    private String type;
+
+    private String value;
+
+    private String state;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac == null ? null : mac.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getClasss() {
+        return classs;
+    }
+
+    public void setClasss(String classs) {
+        this.classs = classs == null ? null : classs.trim();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value == null ? null : value.trim();
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state == null ? null : state.trim();
+    }
+}
---
+++ src/config/test/test.java
@@ -0,0 +1,5 @@
+package config.test;
+
+public class test {
+    
+}

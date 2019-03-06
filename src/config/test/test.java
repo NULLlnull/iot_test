20ce9a83f7d027ee39e96682c486ed88c3287d47
PC <954356394@qq.com>
2019-03-06 14:54:35 +0800

    修复了BUG。增加了新增的Demo

--- src/config/test/test.java
+++ src/config/test/test.java
@@ -1,5 +1,32 @@
 package config.test;
 
+import com.mt.domain.DataConnection;
+import com.mt.domain.Equipment;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class test {
-    
+    SqlSession sqlSession = null;
+    DataConnection dataConnection = new DataConnection();
+
+    @Test
+    public void TestDelete() {
+        try {
+            sqlSession = dataConnection.getSqlSession();
+            Equipment bean = new Equipment();
+            bean.setId(1);
+            bean.setMac("aa-bb-cc-dd-ee-ff");
+            bean.setType("test");
+            sqlSession.insert("com.mt.mapper.EquipmentMapper.insert", bean);
+            sqlSession.commit();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
 }
